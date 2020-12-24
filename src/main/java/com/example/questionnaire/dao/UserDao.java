fb4522c3f82474aeb9a1a501ef345292d76251cf
arcsign <arcsign@163.com>
2020-12-24 18:19:17 +0800

    用户增删改查

--- src/main/java/com/example/questionnaire/dao/UserDao.java
+++ src/main/java/com/example/questionnaire/dao/UserDao.java
@@ -2,7 +2,26 @@ package com.example.questionnaire.dao;
 
 import com.example.questionnaire.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 public interface UserDao extends JpaRepository<User, Integer> {
     User findDistinctByUsername(String username);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update user set email = :#{#user.email}, phone_num = :#{#user.phoneNum}, username = :#{#user.username} where user_id = :#{#user.userId}", nativeQuery = true)
+    int updateUser(User user);
+
+    @Query(value="select u.user_id as userId, u.email, u.phone_num as phoneNum, u.username, ud.dep_id as depId from user u left join user_department ud on u.user_id = ud.user_id where u.status = '0' order by u.user_id desc", nativeQuery = true)
+    List<Map<String, Object>> userLists();
+
+    @Modifying
+    @Transactional
+    @Query(value = "update user set status = 1 where user_id = ?1", nativeQuery = true)
+    int delUser(Integer userId);
 }
---
+++ src/main/java/com/example/questionnaire/service/UserManagementService.java
@@ -0,0 +1,21 @@
+package com.example.questionnaire.service;
+
+import com.example.questionnaire.model.User;
+import net.sf.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public interface UserManagementService {
+
+    //修改用户
+    JSONObject updateUser(User user);
+
+    //删除用户
+    int delUser(Integer id);
+
+    //查询用户
+    List<Map<String, Object>> userLists();
+}
